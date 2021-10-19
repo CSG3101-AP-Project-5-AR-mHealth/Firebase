29a453a8263759cb1dc63e3cafa2e396ed0b8d5f
Craig baldry <cbaldry@our.ecu.edu.au>
2021-10-20 03:57:33 +0800

    update model with confusion matrix

--- api/timeseries_model/create_model_new.py
+++ api/timeseries_model/create_model_new.py
@@ -1,6 +1,8 @@
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, recall_score, accuracy_score, precision_score
 
 import pandas as pd
 from zipfile import ZipFile
@@ -75,56 +77,67 @@ model = keras.Sequential(
     [
         keras.layers.Input(shape=(x_train.shape[1], x_train.shape[2])),
         keras.layers.Conv1D(
-            filters=32, kernel_size=7, padding="same", strides=2, activation="relu"
+            filters=256, kernel_size=7, padding="same", strides=2, activation="relu"
         ),
         keras.layers.Dropout(rate=0.2),
         keras.layers.Conv1D(
-            filters=16, kernel_size=7, padding="same", strides=2, activation="relu"
+            filters=128, kernel_size=7, padding="same", strides=2, activation="relu"
+        ),
+        keras.layers.Dropout(rate=0.2),
+        keras.layers.Conv1D(
+            filters=64, kernel_size=7, padding="same", strides=2, activation="relu"
         ),
         keras.layers.Conv1DTranspose(
-            filters=16, kernel_size=7, padding="same", strides=2, activation="relu"
+            filters=64, kernel_size=7, padding="same", strides=2, activation="relu"
         ),
         keras.layers.Dropout(rate=0.2),
         keras.layers.Conv1DTranspose(
-            filters=32, kernel_size=7, padding="same", strides=2, activation="relu"
+            filters=128, kernel_size=7, padding="same", strides=2, activation="relu"
+        ),
+        keras.layers.Dropout(rate=0.2),
+        keras.layers.Conv1DTranspose(
+            filters=256, kernel_size=7, padding="same", strides=2, activation="relu"
         ),
         keras.layers.Conv1DTranspose(filters=1, kernel_size=7, padding="same"),
     ]
 )
-model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss="mse")
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001), loss="mse")
 model.summary()
 
 history = model.fit(
     x_train,
     x_train,
     epochs=50,
-    batch_size=128,
+    batch_size=256,
     validation_split=0.1,
     callbacks=[
-        keras.callbacks.EarlyStopping(monitor="val_loss")
+        keras.callbacks.EarlyStopping(monitor="val_loss", patience=5, mode="min", verbose=1)
     ],
 )
+
+# Plotting loss to check for overfitting
+fig = plt.figure()
+plt.plot(history.history['loss'], label='Training loss')
+plt.plot(history.history['val_loss'], label='Validation loss')
+plt.ylabel('Loss')
+plt.xlabel('No. Epochs')
+plt.legend()
+plt.show()
 
 
 # Get train MAE loss.
 x_train_pred = model.predict(x_train, verbose=1)
 train_mae_loss = np.mean(np.abs(x_train_pred - x_train), axis=1)
 
-# plt.hist(train_mae_loss, bins=10)
-# plt.xlabel("Train MAE loss")
-# plt.ylabel("No of samples")
-# plt.title("Reconstruction Error Threshold")
-# plt.show()
+plt.hist(train_mae_loss, bins=10)
+plt.xlabel("Train MAE loss")
+plt.ylabel("No of samples")
+plt.title("Reconstruction Error Threshold")
+plt.show()
 
 # Get reconstruction loss threshold.
 threshold = np.max(train_mae_loss)
 print("Reconstruction error threshold: ", threshold)
-
-# Checking how the first sequence is learnt
-# plt.plot(x_train[0])
-# plt.plot(x_train_pred[0])
-# plt.title("Reconstructed")
-# plt.show()
 
 #Prepare test data
 df_test_value = (df_test_data - training_mean) / training_std
@@ -142,28 +155,46 @@ x_test_pred = model.predict(x_test, verbose=1)
 test_mae_loss = np.mean(np.abs(x_test_pred - x_test), axis=1)
 test_mae_loss = test_mae_loss.reshape((-1))
 
-# plt.hist(test_mae_loss, bins=10)
-# plt.xlabel("test MAE loss")
-# plt.ylabel("No of samples")
-# plt.show()
+plt.hist(test_mae_loss, bins=10)
+plt.xlabel("Test MAE loss")
+plt.ylabel("No of samples")
+plt.show()
 
 # Detect all the samples which are anomalies.
 anomalies = test_mae_loss > threshold
 print("Number of anomaly samples: ", np.sum(anomalies))
-print("Indices of anomaly samples: ", np.where(anomalies))
+# print("Indices of anomaly samples: ", np.where(anomalies))
+actualAnomalies = df_test_data > 150
+print("Number of anomaly samples: ", np.sum(actualAnomalies))
+# print("Indices of anomaly samples: ", np.where(actualAnomalies))
 
+conf_matrix = confusion_matrix(actualAnomalies, anomalies)
+plt.figure(figsize=(4, 4))
+sns.heatmap(conf_matrix, annot=True, fmt="d");
+plt.title("Confusion matrix")
+plt.ylabel('True class')
+plt.xlabel('Predicted class')
+plt.show()
+# print Accuracy, precision and recall
+print(" Accuracy: ",accuracy_score(actualAnomalies, anomalies))
+print(" Recall: ",recall_score(actualAnomalies, anomalies))
+print(" Precision: ",precision_score(actualAnomalies, anomalies))
 
 # data i is an anomaly if samples [(i - timesteps + 1) to (i)] are anomalies
-anomalous_data_indices = []
-for data_idx in range(TIME_STEPS - 1, len(df_test_value) - TIME_STEPS + 1):
-    if np.all(anomalies[data_idx - TIME_STEPS + 1 : data_idx]):
-        anomalous_data_indices.append(data_idx)
+# anomalous_data_indices = []
+# for data_idx in range(TIME_STEPS - 1, len(df_test_value) - TIME_STEPS + 1):
+#     if np.all(anomalies[data_idx - TIME_STEPS + 1 : data_idx]):
+#         anomalous_data_indices.append(data_idx)
 
-# df_subset = df_test_data.iloc[anomalous_data_indices]
-# fig, ax = plt.subplots()
-# df_test_data.plot(legend=False, ax=ax)
-# df_subset.plot(legend=False, ax=ax, color="r")
-# plt.show()
+df_subset = df_test_data.iloc[np.where(anomalies)]
+# df_subset1 = df_test_data.iloc[np.where(actualAnomalies)]
+fig, ax = plt.subplots()
+df_test_data.plot(legend=False, ax=ax)
+df_subset.plot(legend=False, ax=ax, color="r")
+# df_subset1.plot(legend=False, ax=ax, color="g")
+plt.xlabel("Date Time")
+plt.ylabel("Heart Rate")
+plt.show()
 
 model.save("saved_model")
 
